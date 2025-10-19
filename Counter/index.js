b7bcecafed928800b256c8d2071ff0d9e476f52c
MosesM21 <101958041+MosesM21@users.noreply.github.com>
2025-10-20 01:15:36 +0200

    Implement Passenger Counter application with HTML, CSS, and JavaScript

---
+++ Counter/index.js
@@ -0,0 +1,15 @@
+    let countEl = document.getElementById("count-el");
+
+function increaseCount() {
+    let count = parseInt(countEl.innerText);
+    count += 1;
+    countEl.innerText = count;
+}
+
+function decreaseCount() {
+    let count = parseInt(countEl.innerText);
+    if (count > 0) {
+        count -= 1;
+        countEl.innerText = count;
+    }
+}
