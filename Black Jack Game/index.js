a2fd30054d44c32253bcd86c896f902af966c10b
MosesM21 <101958041+MosesM21@users.noreply.github.com>
2025-11-02 15:29:00 +0200

    - changed button colour to gold - added functionality to buttons

--- Black Jack Game/index.js
+++ Black Jack Game/index.js
@@ -1,20 +1,44 @@
-let firstCard = 12;
+let firstCard = 2;
 let secondCard = 9;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
-
 let hasBlackJack = false
 let isAlive = true //in the game
+let message = ""
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardEl = document.querySelector("#card-el")
 
-if (sum <= 20) {
-    console.log('Do you want to draw a new card 😒');
-    
-} else if (sum === 21) {
-    console.log('You won!!...BLACKJACK😍😍')
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game!!😭")
-    isAlive = false
+// START GAME
+function startGame() {
+    renderGame()
 }
 
+// RENDER FUNCTION
+function renderGame() {
+    sumEl.textContent = sum
+    cardEl.textContent = cards[0] + " and " + cards[1]
+
+    if (sum <= 20) {
+        message = 'Do you want to draw a new card 😒';
+    
+    } else if (sum === 21) {
+        message = 'You won!!...BLACKJACK😍😍'
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!!😭"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
+
+
+// Get new card button function
+function newCard() {
+    let card = 10; 
+    sum += card
+    cards.push(card)
+    startGame()
+}
 // CASH OUT
-console.log(isAlive)
