69f943f4d549794c7f77ab7453baccc9dd253cf6
Moses Mwale <101958041+MosesM21@users.noreply.github.com>
2026-02-04 11:55:50 +0200

    - added stock status indicator arrows - linked backend to the front-end

--- Stock Ticker/fakeStockApi.js
+++ Stock Ticker/fakeStockApi.js
@@ -1,11 +1,13 @@
 
 function getStockData() {
-    return {
+    const stockData = {
         name: "TechCorp",
         symbol: "TC",
-        price: Math.random() * 1000,
+        price: `K${(Math.random() * 1000).toFixed(2)}`,
         time: new Date().toLocaleTimeString(),
     }
+    console.log(stockData);
+    return stockData;
 }
 
-export { getStockData };
+export { getStockData }
--- Stock Ticker/script.js
+++ Stock Ticker/script.js
@@ -1,14 +1,43 @@
 import { getStockData } from './fakeStockApi.js';
 
+const stockNameElem = document.getElementById('stock-name');
+const stockSymbolElem = document.getElementById('stock-symbol');
+const stockPriceElem = document.getElementById('stock-price');
+const stockTimeElem = document.getElementById('stock-time');
 
+function updateStockInfo() {
+    const stockData = getStockData();
+    stockNameElem.textContent = stockData.name;
+    stockSymbolElem.textContent = stockData.symbol;
+    stockPriceElem.textContent = stockData.price;
+    stockTimeElem.textContent = stockData.time;
 
-function renderStockTicker(stockData) {
-    const stockDisplayName = document.getElementById('stock-name');
-    const stockDisplaySymbol = document.getElementById('stock-symbol');
-    const stockDisplayPrice = document.getElementById('stock-price');
-    const stockDisplayPriceIcon = document.getElementById('stock-price-arrow');
-
-    stockDisplayName.textContent = getStockData().name;
+    updateStockPriceArrow(parseFloat(stockData.price.slice(1)), parseFloat(stockData.price.slice(1)));
+    return stockData;
 }
+
+function updateStockPriceArrow(oldStockPrice, newPrice) { 
+    const stockPriceArrowElem = document.getElementById('stock-price-arrow');
+    const stockData = getStockData();
+    
+    oldStockPrice = parseFloat(stockData.price.slice(1));
+
+    if (newPrice > oldStockPrice) {
+        stockPriceArrowElem.textContent = '▲';
+        stockPriceArrowElem.style.color = 'limegreen';
+    } else if (newPrice < oldStockPrice) {
+        stockPriceArrowElem.textContent = '▼';
+        stockPriceArrowElem.style.color = 'red';
+    } else {
+        stockPriceArrowElem.textContent = '►';
+        stockPriceArrowElem.style.color = 'gray';
+    }
+    return newPrice;
+}
+setInterval(updateStockInfo, 5000);
+updateStockInfo();
+let newPrice = parseFloat(getStockData().price.slice(1));
+
+
 
 
