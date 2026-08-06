ef0957df12179670740eb875f415d4da2458a8f6
Moses Mwale <mwalemoses54@gmail.com>
2026-08-06 16:47:42 +0200

    Reduce: Add total rooms accumulator

--- Property Card/properties.js
+++ Property Card/properties.js
@@ -4,6 +4,7 @@ export const properties = [
     price: "$1,000,000",
     description: "Highly sought after property in the heart of London.",
     area: "62m²",
+    rooms: [3, 2, 1, 1],
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSClYiRPA7s-RIYC8qyhq42Rr8taoQG_SgEvxYIGYHXcA&s=10",
   },
@@ -13,6 +14,7 @@ export const properties = [
     description:
       "The official residence and workplace of the President of the United States.",
     area: "5,000m²",
+    rooms: [16, 35, 132, 11],
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0Tl9j28U-6FHy8jPanwmrH0QSNSl4tvHyhCWLWdFEzw&s",
   },
@@ -22,6 +24,7 @@ export const properties = [
     description:
       "A historic palace known for its opulent architecture and gardens.",
     area: "63,154m²",
+    rooms: [20, 45, 150, 25],
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJYjc11Ygt9YOj7tOad6F1enE25BWSC-0sPfFv9PxSnco_g0Gw2TlZNEc&s=10",
   },
