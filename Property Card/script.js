200337d903c11e763f38c0189cb7984a93e53763
Moses Mwale <mwalemoses54@gmail.com>
2026-08-06 16:47:42 +0200

    Reduce: Add total rooms accumulator

--- Property Card/script.js
+++ Property Card/script.js
@@ -3,12 +3,16 @@ import { properties } from "./properties.js";
 let propertyContainer = document.getElementById("property-container");
 
 function getPropertyHtml(property) {
+  const totalRooms = property.rooms.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
   return `
     <div class="property-card">
       <img src="${property.image}" alt="${property.name}" />
       <div class="property-details">  
         <h2>${property.name}</h2>
         <p>Price: ${property.price}</p>
+        <p>Rooms: ${totalRooms}</p>
         <p>Description: ${property.description}</p>
         <p>Area: ${property.area}</p>
       </div>
